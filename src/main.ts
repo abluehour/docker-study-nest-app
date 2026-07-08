f087eec1b16eee5a1effd94846ca7981df2a4cfa
Jung-YoonSeong <abluehour@gmail.com>
2026-07-08 20:42:34 +0900

    feat:  CORS 에러 방지 설정

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// backend/src/main.ts (CORS 활성화 구성)
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
 
   app.enableCors({
-    origin: true, // 모든 도메인(localhost 포함)에서의 요청을 허용
+    origin: [
+      "https://abluehour.croffledev.kr", // 통신을 허용할 프론트엔드 도메인 출처 명시
+    ],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 bootstrap().catch((err) => {
   console.error('Error starting the application:', err);
