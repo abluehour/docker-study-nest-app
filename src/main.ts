629379f8cd21aea6d371e52eb3fe4e2732941984
Jung-YoonSeong <abluehour@gmail.com>
2026-07-12 14:42:01 +0900

    fix: https로 변경

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      "http://8.231.212.239:8080", // 통신을 허용할 프론트엔드 도메인 출처 명시
+      "https://abluehour.kro.kr", // 통신을 허용할 프론트엔드 도메인 출처 명시
     ],
     credentials: true,
   });
